2c2db644bfcf0c16fea8cf468abe44f608231b0e
Alejandro Montals Pozzoli <amontals83.work@gmail.com>
2023-11-29 12:37:34 +0100

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace CargaSkipTracing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.btnFichero = new System.Windows.Forms.Button();
+            this.txtFichero = new System.Windows.Forms.TextBox();
+            this.cboCartera = new System.Windows.Forms.ComboBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(221, 100);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(75, 23);
+            this.btnCargar.TabIndex = 27;
+            this.btnCargar.Text = "Guardar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(9, 69);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(42, 13);
+            this.Label3.TabIndex = 26;
+            this.Label3.Text = "Fichero";
+            // 
+            // btnFichero
+            // 
+            this.btnFichero.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFichero.Location = new System.Drawing.Point(267, 65);
+            this.btnFichero.Name = "btnFichero";
+            this.btnFichero.Size = new System.Drawing.Size(29, 23);
+            this.btnFichero.TabIndex = 25;
+            this.btnFichero.Text = "...";
+            this.btnFichero.UseVisualStyleBackColor = true;
+            // 
+            // txtFichero
+            // 
+            this.txtFichero.Location = new System.Drawing.Point(79, 66);
+            this.txtFichero.Name = "txtFichero";
+            this.txtFichero.Size = new System.Drawing.Size(183, 20);
+            this.txtFichero.TabIndex = 24;
+            // 
+            // cboCartera
+            // 
+            this.cboCartera.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboCartera.FormattingEnabled = true;
+            this.cboCartera.Location = new System.Drawing.Point(80, 24);
+            this.cboCartera.Name = "cboCartera";
+            this.cboCartera.Size = new System.Drawing.Size(218, 21);
+            this.cboCartera.TabIndex = 23;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(10, 27);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(41, 13);
+            this.Label1.TabIndex = 22;
+            this.Label1.Text = "Cartera";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 143);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.btnFichero);
+            this.Controls.Add(this.txtFichero);
+            this.Controls.Add(this.cboCartera);
+            this.Controls.Add(this.Label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button btnCargar;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Button btnFichero;
+        internal System.Windows.Forms.TextBox txtFichero;
+        internal System.Windows.Forms.ComboBox cboCartera;
+        internal System.Windows.Forms.Label Label1;
+    }
+}
+
